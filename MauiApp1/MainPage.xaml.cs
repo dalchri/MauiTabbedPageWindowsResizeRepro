66baa4736b0ede68f703878a5128a346d5d5fefc
Chris Dalla Piazza <dalchri@outlook.com>
2024-01-20 09:11:12 -0500

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPageButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new MainPage();
+        }
+
+        private void TabbedPageButton_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new TabbedPage();
+        }
+    }
+
+}
---
+++ MauiApp1/TabbedPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class TabbedPage : Microsoft.Maui.Controls.TabbedPage
+    {
+        int count = 0;
+
+        public TabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TabbedPage_SizeChanged(object sender, EventArgs e)
+        {
+            //Task.Delay(1000).ContinueWith(t =>
+            //{
+            //    this.Dispatcher.Dispatch(() =>
+            //    {
+            //        this.ForceLayout();
+            //        this.UpdateChildrenLayout();
+                    
+            //        foreach(var c in this.Children)
+            //        {
+            //            c.ForceLayout();
+            //            this.UpdateChildrenLayout();
+            //            Debug.WriteLine("Layout " + DateTime.Now.Ticks);
+            //        }
+            //    });
+            //});
+        }
+    }
+
+}
